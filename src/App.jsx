8bb72234214869770c4b8a439dfc99ef8bd11a22
jv <joaovictor.iteck@gmail.com>
2026-06-30 14:49:03 -0300

    Restaura codigo ao estado anterior aos testes (igual ao commit 4a6ba81): remove typeTest.ts e import

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@
 import { useScroll, useTransform } from 'framer-motion';
 import { useSmoothScroll } from './hooks/useSmoothScroll';
 import { useScrollState } from './hooks/useScrollState';
-import './typeTest.ts';
 
 // Components
 import Navbar from './components/Navbar';
--- src/typeTest.ts
+++
@@ -1,4 +0,0 @@
-// Arquivo de teste: erro de sintaxe proposital para quebrar o build
-const quantidade: number = ;
-
-export default quantidade;
